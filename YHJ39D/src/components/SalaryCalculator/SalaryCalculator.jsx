c64e2fd32c61985b4d703f5040006f978dae6c43
VAdam02 <adam.vilanyi2@gmail.com>
2024-04-05 18:51:57 +0200

    Better calculateNetSalary

--- YHJ39D/src/components/SalaryCalculator/SalaryCalculator.jsx
+++ YHJ39D/src/components/SalaryCalculator/SalaryCalculator.jsx
@@ -19,24 +19,25 @@ import NetSalary from "./components/NetSalary"
 import Under25YearsSZJA from "./components/Under25YearsSZJA"
 
 const SalaryCalculator = () => {
-  const [name, setName] = useState("Bendi");
-  const [grossSalary, setGrossSalary] = useState(100000);
-  const [isUnder25YearsSZJA, setUnder25YearsSZJA] = useState(false);
-  const [isFreshMarried, setFreshMarried] = useState(false);
-  const [familyTaxCredit, setFamilyTaxCredit] = useState(false);
-  const [dependent, setDependent] = useState(0);
-  const [dependentBeneficiary, setDependentBeneficiary] = useState(0);
-  const [netSalary, setNetSalary] = useState(calculateNetSalary(grossSalary, isUnder25YearsSZJA, isFreshMarried, dependent, dependentBeneficiary));
+  const [name_, setName] = useState("Bendi");
+  const [grossSalary_, setGrossSalary] = useState(100000);
+  const [isUnder25YearsSZJA_, setUnder25YearsSZJA] = useState(false);
+  const [isFreshMarried_, setFreshMarried] = useState(false);
+  const [isFamilyTaxCredit_, setFamilyTaxCredit] = useState(false);
+  const [dependent_, setDependent] = useState(0);
+  const [dependentBeneficiary_, setDependentBeneficiary] = useState(0);
+  const [netSalary_, setNetSalary] = useState(calculateNetSalary(grossSalary_, isUnder25YearsSZJA_, isFreshMarried_, dependent_, dependentBeneficiary_));
 
-  function calculateNetSalary(grossSalary, isUnder25YearsSZJA, isFreshMarried, dependent, dependentBeneficiary) {
+  function calculateNetSalary({grossSalary = grossSalary_, isUnder25YearsSZJA = isUnder25YearsSZJA_, isFreshMarried = isFreshMarried_, isFamilyTaxCredit = isFamilyTaxCredit_, dependent = dependent_, dependentBeneficiary = dependentBeneficiary_}) {
     let szja = (isUnder25YearsSZJA ? Math.max(0, grossSalary - 499952) : grossSalary) * 0.15;
     let tb = grossSalary * 0.185;
     let bonus = isFreshMarried ? 5000 : 0;
-    switch (dependentBeneficiary) {
-      case 1: bonus += 10000*dependent; break;
-      case 2: bonus += 20000*dependent; break;
-      case 3: bonus += 33000*dependent; break;
-    }
+    if (isFamilyTaxCredit) {
+      switch (dependentBeneficiary) {
+        case 1: bonus += 10000*dependent; break;
+        case 2: bonus += 20000*dependent; break;
+        case 3: bonus += 33000*dependent; break;
+    }}
     return grossSalary - szja - tb + bonus;
   }
 
@@ -48,50 +49,50 @@ const SalaryCalculator = () => {
     let grossSalary = parseInt(event.target.value || 0);
     if (isNaN(grossSalary)) return;
     setGrossSalary(grossSalary);
-    setNetSalary(calculateNetSalary(grossSalary, isUnder25YearsSZJA, isFreshMarried, dependent, dependentBeneficiary));
+    setNetSalary(calculateNetSalary({grossSalary: grossSalary}));
   }
 
   function handleSliderSalaryChange([grossSalary]) {
     setGrossSalary(grossSalary);
-    setNetSalary(calculateNetSalary(grossSalary), isUnder25YearsSZJA, isFreshMarried, dependent, dependentBeneficiary);
+    setNetSalary(calculateNetSalary({grossSalary: grossSalary}));
   }
 
   function handleButtonSalaryChange(event) {
-    let newGrossSalary = grossSalary;
+    let newGrossSalary = grossSalary_;
     switch (event.target.innerText) {
       case "-5%":
-        newGrossSalary = grossSalary * 0.95;
+        newGrossSalary = grossSalary_ * 0.95;
         break;
       case "-1%":
-        newGrossSalary = grossSalary * 0.99;
+        newGrossSalary = grossSalary_ * 0.99;
         break;
       case "+1%":
-        newGrossSalary = grossSalary * 1.01;
+        newGrossSalary = grossSalary_ * 1.01;
         break;
       case "+5%":
-        newGrossSalary = grossSalary * 1.05;
+        newGrossSalary = grossSalary_ * 1.05;
         break;
     }
     setNetSalary
     setGrossSalary(newGrossSalary);
-    setNetSalary(calculateNetSalary(newGrossSalary, isUnder25YearsSZJA, isFreshMarried, dependent, dependentBeneficiary));
+    setNetSalary(calculateNetSalary({grossSalary: newGrossSalary}));
   }
 
   function handleUnder25YearsSZJAChange(isUnder25YearsSZJA) {
     setUnder25YearsSZJA(isUnder25YearsSZJA);
-    setNetSalary(calculateNetSalary(grossSalary, isUnder25YearsSZJA, isFreshMarried, dependent, dependentBeneficiary));
+    setNetSalary(calculateNetSalary({isUnder25YearsSZJA: isUnder25YearsSZJA}));
   }
 
   function handleFreshMarriedChange(isFreshMarried) {
     setFreshMarried(isFreshMarried);
-    setNetSalary(calculateNetSalary(grossSalary, isUnder25YearsSZJA, isFreshMarried, dependent, dependentBeneficiary));
+    setNetSalary(calculateNetSalary({isFreshMarried: isFreshMarried}));
   }
 
   function handleFamilyTaxCreditChange(isFamilyTaxCredit, dependent, dependentBeneficiary) {
     setFamilyTaxCredit(isFamilyTaxCredit);
     setDependent(dependent);
     setDependentBeneficiary(dependentBeneficiary);
-    setNetSalary(calculateNetSalary(grossSalary, isUnder25YearsSZJA, isFreshMarried, dependent, dependentBeneficiary));
+    setNetSalary(calculateNetSalary({isFamilyTaxCredit: isFamilyTaxCredit, dependent: dependent, dependentBeneficiary: dependentBeneficiary}));
   }
 
   return (
@@ -102,14 +103,14 @@ const SalaryCalculator = () => {
     </div>
     <Card>
       <CardHeader>
-        <CardTitle>{name} BÉRÉNEK KISZÁMÍTÁSA</CardTitle>
+        <CardTitle>{name_} BÉRÉNEK KISZÁMÍTÁSA</CardTitle>
       </CardHeader>
       <CardContent>
-        <TextboxWithLabel label="Családtag neve" description="Add meg a családtag nevét!" placeholder="Bendi" value={name} onChange={handleNameChange} />
+        <TextboxWithLabel label="Családtag neve" description="Add meg a családtag nevét!" placeholder="Bendi" value={name_} onChange={handleNameChange} />
 
-        <TextboxWithLabel label="Bruttó bér" description="Add meg a bruttó béredet!" placeholder="100000 Ft" value={grossSalary} onChange={handleEventGrossSalaryChange} />
+        <TextboxWithLabel label="Bruttó bér" description="Add meg a bruttó béredet!" placeholder="100000 Ft" value={grossSalary_} onChange={handleEventGrossSalaryChange} />
 
-        <Slider min={0} max={1000000} step={1} value={[grossSalary]} onValueChange={handleSliderSalaryChange} /><br />
+        <Slider min={0} max={1000000} step={1} value={[grossSalary_]} onValueChange={handleSliderSalaryChange} /><br />
         <Button variant="outline" onClick={handleButtonSalaryChange}>-5%</Button>
         <Button variant="outline" onClick={handleButtonSalaryChange}>-1%</Button>
         <Button variant="outline" onClick={handleButtonSalaryChange}>+1%</Button>
@@ -117,14 +118,14 @@ const SalaryCalculator = () => {
         <br />
 
         <big>Kedvezmények</big><br />
-        <Under25YearsSZJA checked={isUnder25YearsSZJA} onCheckedChange={handleUnder25YearsSZJAChange} />
-        <FreshMarried checked={isFreshMarried} onPermittedChange={handleFreshMarriedChange} />
+        <Under25YearsSZJA checked={isUnder25YearsSZJA_} onCheckedChange={handleUnder25YearsSZJAChange} />
+        <FreshMarried checked={isFreshMarried_} onPermittedChange={handleFreshMarriedChange} />
         <Switch id="personalTaxCredit" /><Label htmlFor="personalTaxCredit">Személyes adókedvezmény</Label><br />
-        <FamilyTaxCredit checked={familyTaxCredit} dependentNumber={dependent} dependentBeneficiaryNumber={dependentBeneficiary} maxDependentBeneficiary={3} onChange={handleFamilyTaxCreditChange} />
+        <FamilyTaxCredit checked={isFamilyTaxCredit_} dependentNumber={dependent_} dependentBeneficiaryNumber={dependentBeneficiary_} maxDependentBeneficiary={3} onChange={handleFamilyTaxCreditChange} />
 
       </CardContent>
       <CardFooter>
-        <NetSalary netSalary={netSalary} />
+        <NetSalary netSalary={netSalary_} />
       </CardFooter>
     </Card>
   </div>);

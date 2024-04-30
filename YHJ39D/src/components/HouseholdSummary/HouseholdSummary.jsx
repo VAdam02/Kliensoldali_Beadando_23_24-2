060756e23e0b5de4fc1d5204c8b32ec737d2aed4
VAdam02 <adam.vilanyi2@gmail.com>
2024-04-30 20:37:21 +0200

    Summary table

--- YHJ39D/src/components/HouseholdSummary/HouseholdSummary.jsx
+++ YHJ39D/src/components/HouseholdSummary/HouseholdSummary.jsx
@@ -1,5 +1,47 @@
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Table } from "@/components/ui/table"
+
 const HouseholdSummary = () => {
-  return <div>HouseholdSummary</div>;
+  const dispatch = useDispatch();
+  const people = useSelector(state => state.people.people);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Household Summary</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Gross Salary</th>
+              <th>Net Salary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {people.map(person => (
+              <tr key={person.id}>
+                <td>{person.name}</td>
+                <td>{person.grossSalary}</td>
+                <td>{person.netSalary}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </CardContent>
+      <CardFooter>
+        <p>Total net salary: {people.reduce((acc, person) => acc + person.netSalary, 0)}</p>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default HouseholdSummary;
